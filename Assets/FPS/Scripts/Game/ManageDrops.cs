9b3c9628c1ebf4af82e7b2fdc822d4c5a0109c17
carlosiglesias1 <carlosig490@gmail.com>
2022-03-08 16:58:30 +0100

    eliminar tutoriales

---
+++ Assets/FPS/Scripts/Game/ManageDrops.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageDrops : MonoBehaviour
+{
+    public List<GameObject> dropOptions;
+    public List<GameObject> enemies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
